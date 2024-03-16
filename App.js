c560cab0add6453f98b422f99bdfab193156b15e
hxrda <henein.roda@gmail.com>
2024-03-16 06:35:26 +0200

    Exercise complete

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, TextInput, View, Button } from "react-native";
+import * as Speech from "expo-speech";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	//States:
+	const [text, setText] = useState("");
+
+	//Functions:
+	const speak = () => {
+		Speech.speak(text);
+	};
+
+	//Rendering:
+	return (
+		<View style={styles.container}>
+			<TextInput
+				style={styles.textInputContainer}
+				placeholder="Type text"
+				value={text}
+				onChangeText={(text) => setText(text)}
+			/>
+			<View style={{ margintTop: 5 }}></View>
+			<Button title="Press to hear text" onPress={speak} />
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+
+	textInputContainer: {
+		width: 200,
+		height: 40,
+		borderColor: "gray",
+		borderWidth: 1,
+		marginBottom: 10,
+	},
 });
